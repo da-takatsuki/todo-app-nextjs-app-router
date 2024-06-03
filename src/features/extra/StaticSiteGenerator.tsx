85a6956bdbf5fab9e3029403571be4c52e531432
da-takatsuki <da-takatsuki@kddi.com>
2024-06-03 13:40:17 +0900

    とりあえずtrycatchで囲ってみる

--- src/features/extra/StaticSiteGenerator.tsx
+++ src/features/extra/StaticSiteGenerator.tsx
@@ -2,24 +2,30 @@ import { HOST_NAME } from "@/utils/variables";
 import Image from "next/image";
 
 export default async function StaticSiteGenerator() {
-  const res = await fetch(`${HOST_NAME}/api/pokemon`);
-  const data = await res.json();
+  let data;
+  try {
+    const res = await fetch(`${HOST_NAME}/api/pokemon`);
+    data = await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 
   return (
     <div>
       <h1>Pokémon List</h1>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {data.data.map((pokemon: any, index: any) => (
-          <div key={index} style={{ margin: "10px", textAlign: "center" }}>
-            <Image
-              src={pokemon.image}
-              width={200}
-              height={200}
-              alt={pokemon.name}
-            />
-            <p>{pokemon.name}</p>
-          </div>
-        ))}
+        {data &&
+          data.data.map((pokemon: any, index: any) => (
+            <div key={index} style={{ margin: "10px", textAlign: "center" }}>
+              <Image
+                src={pokemon.image}
+                width={200}
+                height={200}
+                alt={pokemon.name}
+              />
+              <p>{pokemon.name}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
