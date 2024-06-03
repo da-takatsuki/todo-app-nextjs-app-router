14e7d66903f0f9e590733e0ad8b32c48ac394e90
da-takatsuki <da-takatsuki@kddi.com>
2024-06-03 13:49:37 +0900

    明示的につける

--- src/features/extra/StaticSiteGenerator.tsx
+++ src/features/extra/StaticSiteGenerator.tsx
@@ -2,30 +2,24 @@ import { HOST_NAME } from "@/utils/variables";
 import Image from "next/image";
 
 export default async function StaticSiteGenerator() {
-  let data;
-  try {
-    const res = await fetch(`${HOST_NAME}/api/pokemon`);
-    data = await res.json();
-  } catch (error) {
-    console.log(error);
-  }
+  const res = await fetch(`${HOST_NAME}/api/pokemon`, { cache: "force-cache" });
+  const data = await res.json();
 
   return (
     <div>
       <h1>Pokémon List</h1>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {data &&
-          data.data.map((pokemon: any, index: any) => (
-            <div key={index} style={{ margin: "10px", textAlign: "center" }}>
-              <Image
-                src={pokemon.image}
-                width={200}
-                height={200}
-                alt={pokemon.name}
-              />
-              <p>{pokemon.name}</p>
-            </div>
-          ))}
+        {data.data.map((pokemon: any, index: any) => (
+          <div key={index} style={{ margin: "10px", textAlign: "center" }}>
+            <Image
+              src={pokemon.image}
+              width={200}
+              height={200}
+              alt={pokemon.name}
+            />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
