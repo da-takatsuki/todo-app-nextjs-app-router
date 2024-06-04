27faf53c67830a1d317493eaf7501c672ca6da0b
da-takatsuki <da-takatsuki@kddi.com>
2024-06-04 12:49:59 +0900

    一旦ip認証をコメントアウト

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,30 +6,30 @@ const IP_WHITELIST = [process.env.MY_WIFI_IP1, process.env.MY_WIFI_IP2];
 export async function middleware(request: NextRequest) {
   const res = NextResponse.next();
 
-  // access-deniedページにはミドルウェアを適用しない
-  if (request.nextUrl.pathname === "/access-denied") {
-    return res;
-  }
+  // // access-deniedページにはミドルウェアを適用しない
+  // if (request.nextUrl.pathname === "/access-denied") {
+  //   return res;
+  // }
 
-  // ipアドレスを取得
-  let ip: string = request.ip ?? request.headers.get("x-real-ip") ?? "";
+  // // ipアドレスを取得
+  // let ip: string = request.ip ?? request.headers.get("x-real-ip") ?? "";
 
-  // プロキシ経由の場合
-  const forwardedFor = request.headers.get("x-forwarded-for");
+  // // プロキシ経由の場合
+  // const forwardedFor = request.headers.get("x-forwarded-for");
 
-  // プロキシ経由の場合は、プロキシのIPアドレスを取得
-  if (!ip && forwardedFor) {
-    ip = forwardedFor.split(",").at(0) ?? "Unknown";
-  }
+  // // プロキシ経由の場合は、プロキシのIPアドレスを取得
+  // if (!ip && forwardedFor) {
+  //   ip = forwardedFor.split(",").at(0) ?? "Unknown";
+  // }
 
-  // 取得したIPアドレスがホワイトリストに含まれているかチェックし、含まれていない場合はアクセス拒否
-  if (!IP_WHITELIST.includes(ip)) {
-    console.log("IP_WHITELIST", IP_WHITELIST);
-    console.log("ip", ip);
-    return NextResponse.redirect(
-      "https://middleware-ip-limit-one.vercel.app/access-denied"
-    ); // アクセス拒否のページにリダイレクト(フルパスで指定)
-  }
+  // // 取得したIPアドレスがホワイトリストに含まれているかチェックし、含まれていない場合はアクセス拒否
+  // if (!IP_WHITELIST.includes(ip)) {
+  //   console.log("IP_WHITELIST", IP_WHITELIST);
+  //   console.log("ip", ip);
+  //   return NextResponse.redirect(
+  //     "https://middleware-ip-limit-one.vercel.app/access-denied"
+  //   ); // アクセス拒否のページにリダイレクト(フルパスで指定)
+  // }
 
   return res;
 }
